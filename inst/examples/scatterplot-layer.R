b1ad2db77e62bbfe69b797e7ed4100f7b2377064
skuethe <crazycapivara@gmail.com>
2019-11-06 21:59:50 +0100

    Update example

--- inst/examples/scatterplot-layer.R
+++ inst/examples/scatterplot-layer.R
@@ -3,7 +3,7 @@ library("rdeck")
 rdeck(
   script = system.file("scripts/test.js", package = "rdeck"),
   data = geojsonio::us_cities,
-  longitude = -99.74,
-  latitude = 32.45,
-  zoom = 7
+  longitude = -96.97,
+  latitude = 32.86,
+  zoom = 9
 )
--- inst/scripts/test.js
+++ inst/scripts/test.js
@@ -1,13 +1,24 @@
+/* Multiple layers */
 function deckScript(map, data) {
   console.log("map:", map, "data:", data);
 
-  const scatterplot = new ScatterplotLayer({
+  const scatterplotLayer = new ScatterplotLayer({
     data: data,
     getPosition: d => [d.long, d.lat],
     getRadius: 1000,
-    getFillColor: [250, 140, 10],
+    getFillColor: [0, 0, 140],
     filled: true
   });
 
-  map.setProps({ layers: [ scatterplot ] });
+  const textLayer = new TextLayer({
+    data: data,
+    getPosition: d => [d.long, d.lat],
+    getText: d => d.name,
+    getSize: 25,
+    getColor: [255, 150, 30]
+  });
+
+  map.setProps({
+    layers: [ scatterplotLayer, textLayer ]
+  });
 }
