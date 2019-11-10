9a317005d4d6662f4f8ee2e2191c4c401de35c65
skuethe <crazycapivara@gmail.com>
2019-11-10 13:23:14 +0100

    Refactor

--- R/widget.R
+++ R/widget.R
@@ -19,7 +19,6 @@ make_widget <- function(script, data, web_gl_context = "deck", options = NULL,
     package = "rdeck",
     elementId = element_id
   )
-
 }
 
 #' Create a widget object
@@ -36,7 +35,6 @@ make_widget <- function(script, data, web_gl_context = "deck", options = NULL,
 #' @param element_id (optional) The unique id of the widget.
 #' @param ... Additional properties (picking radius, ...) that are passed to the Deck class.
 #'
-#' @import htmlwidgets
 #' @export
 r2deck <- function(script, data, lng = -122.45, lat = 37.8, zoom = 8, map_style = get_carto_style(),
                    options = NULL, width = NULL, height = NULL, element_id = NULL, ...) {
