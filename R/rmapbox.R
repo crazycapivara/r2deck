743acb875855b08a0b1f6789a184aa8bb534816e
skuethe <crazycapivara@gmail.com>
2019-11-10 11:23:48 +0100

    Add rdocs

--- R/rmapbox.R
+++ R/rmapbox.R
@@ -22,6 +22,21 @@ make_widget <- function(script, data, web_gl_context = "deck", options = NULL,
 
 }
 
+#' Create a widget object
+#'
+#' @param script The name of the JavaScript file containing the vizualisation function.
+#' @param data The data that is passed to the vizualisation function.
+#' @param longitude ...
+#' @param latitude ...
+#' @param zoom ...
+#' @param map_style ...
+#' @param options (optional) Additional options that are passed to the vizualisation function as parameter \code{options}.
+#' @param width (optional) The width of the widget.
+#' @param height (optional) The height of the widget.
+#' @param element_id (optional) The unique id of the widget.
+#' @param ... Additional properties (picking radius, ...) that are passed to the Deck class.
+#'
+#' @import htmlwidgets
 #' @export
 r2deck <- function(script, data, longitude = -122.45, latitude = 37.8, zoom = 8, map_style = get_carto_style(),
                    options = NULL, width = NULL, height = NULL, element_id = NULL, ...) {
@@ -41,6 +56,11 @@ r2deck <- function(script, data, longitude = -122.45, latitude = 37.8, zoom = 8,
   )
 }
 
+#' Create a widget object using the Mapbox WebGL context
+#'
+#' @inheritParams r2deck
+#' @param center ...
+#' @param style The style definition of the map conforming to the Mapbox Style Specification.
 #' @export
 r2mapbox <- function(script, data, center = c(-122.45, 37.8), zoom = 8, style = get_carto_style("dark-matter"),
                      options = NULL, width = NULL, height = NULL, element_id = NULL, ...) {
